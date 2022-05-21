b8a20161fcf9c884f094e9e6a8091e559eef0b68
zsm <zhang_shuming@foxmail.com>
2022-05-21 22:15:02 +0800

    Add a function of querying uploaded image records and optimize page layout

--- src/main/java/com/medirec/controller/ImageController.java
+++ src/main/java/com/medirec/controller/ImageController.java
@@ -1,20 +1,24 @@
 package com.medirec.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.medirec.entity.Image;
 import com.medirec.entity.User;
+import com.medirec.service.ImageService;
+import com.medirec.utils.JsonResponse;
 import com.medirec.utils.SessionUtils;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.stereotype.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import com.medirec.utils.JsonResponse;
-import com.medirec.service.ImageService;
-import com.medirec.entity.Image;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.List;
 
 
 /**
@@ -56,12 +60,11 @@ public class ImageController {
 //        }
 //        return "userhome";
 //    }
-
     @RequestMapping(value = "/uploadAndReconstructImage", method = RequestMethod.POST)
     @ResponseBody
     public JsonResponse uploadAndReconstructImage(MultipartFile file,
-                                            Boolean sinogram,
-                                            HttpServletRequest request) throws IOException {
+                                                  Boolean sinogram,
+                                                  HttpServletRequest request) throws IOException {
         User loginUser = SessionUtils.getLoginUserFromSession();
         System.out.println(loginUser);
         Image uploadedImage = null;
@@ -88,6 +91,16 @@ public class ImageController {
         System.out.println("$$restoring");
 
         return JsonResponse.success("1");
+    }
+
+    @RequestMapping("/records")
+    @ResponseBody
+    public List<Image> getUploadRecords() {
+        User loginUser = SessionUtils.getLoginUserFromSession();
+        List<Image> imageList = imageService.list(
+                new QueryWrapper<Image>().lambda().eq(Image::getUserId, loginUser.getId()).orderByDesc(Image::getUploadTime)
+        );
+        return imageList;
     }
 
     /**
