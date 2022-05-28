c0641a19008187ad6293790b7f9bb219fb472fe9
zsm <zhang_shuming@foxmail.com>
2022-05-29 02:37:52 +0800

    Add content to algorithm demo page and intercept /user/home without login

--- src/main/java/com/medirec/controller/UserController.java
+++ src/main/java/com/medirec/controller/UserController.java
@@ -48,6 +48,9 @@ public class UserController {
 
     @RequestMapping("/home")
     public String redirectToUserHome() {
+        if (SessionUtils.getLoginUserFromSession() == null) {
+            return "redirect:/login";
+        }
         return "userhome";
     }
 
